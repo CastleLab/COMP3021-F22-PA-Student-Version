d667b211f7014ed0bb666cc05098dd06c81fc0fd
Henry Chu <henrytech@outlook.com>
2022-08-01 21:03:43 +0800

    Added some fields to GameState

--- src/main/java/hk/ust/comp3021/game/GameState.java
+++ src/main/java/hk/ust/comp3021/game/GameState.java
@@ -1,8 +1,24 @@
 package hk.ust.comp3021.game;
 
+import hk.ust.comp3021.actions.Action;
+import hk.ust.comp3021.utils.NotImplementedException;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Stack;
+
 /**
  * The state of the Sokoban Game.
  */
-public interface GameState {
+public class GameState {
 
+    private GameBoard board;
+
+    private final Stack<Action> actions = new Stack<>();
+
+    private final Set<Position> destinations = new HashSet<>();
+
+    public boolean isWin() {
+        throw new NotImplementedException();
+    }
 }
