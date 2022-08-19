5f68a3e78da20a5a6433531478135fd503e388d0
Henry Chu <henrytech@outlook.com>
2022-08-19 12:34:34 +0800

    Added test dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,8 +34,9 @@ buildscript {
 
 dependencies {
     compileOnly("org.jetbrains:annotations:23.0.0")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
+    testImplementation("org.mockito:mockito-core:4.7.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
 }
 
 tasks {
