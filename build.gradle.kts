973867fd2a295ad0ff6b2d6ca45c9f0952145832
William Aaron Cheung <troublor@live.com>
2022-11-04 23:14:59 +0800

    working on customizing test runner

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,7 +39,9 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
     testImplementation("org.junit.jupiter:junit-jupiter-params:5.9.0")
     testImplementation("org.mockito:mockito-core:4.7.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
+    testImplementation("org.mockito:mockito-junit-jupiter:4.7.0")
+    testImplementation("org.junit.platform:junit-platform-launcher:1.9.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.9.0")
 }
 
 checkstyle {
